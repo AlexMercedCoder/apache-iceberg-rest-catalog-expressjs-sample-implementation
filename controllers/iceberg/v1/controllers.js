2e761bbab73e4b53449d1e5b122bce940c123a46
Alex Merced <alex@alexmerced.dev>
2022-12-27 14:36:33 -0500

    controller functions layed out

--- controllers/iceberg/v1/controllers.js
+++ controllers/iceberg/v1/controllers.js
@@ -1,5 +1,7 @@
 // controllers functions for routes under "iceberg/v1"
 
+
+// "/config"
 export const v1_config = (req, res) => {
     // configs object
     const configs = {
@@ -10,3 +12,68 @@ export const v1_config = (req, res) => {
     // send configs as json
     res.json(configs)
 }
+
+// "/oauth/tokens"
+export const v1_oauth_tokens = (req, res => {
+    res.json({message: "/oauth/tokens"})
+})
+
+// "/:prefix/namespaces (get)"
+export const v1_prefix_namespaces_get = (req, res => {
+    res.json({message: "/:prefix/namespaces (get)"})
+})
+
+// "/:prefix/namespaces (post)"
+export const v1_prefix_namespaces_post = (req, res => {
+    res.json({message: "/:prefix/namespaces (post)"})
+})
+
+// "/:prefix/namespaces/:namespace (get)"
+export const v1_prefix_namespaces_namespace_get = (req, res => {
+    res.json({message: "/:prefix/namespaces/:namespace (get)"})
+})
+
+// "/:prefix/namespaces/:namespace (delete)"
+export const v1_prefix_namespaces_namespace_delete = (req, res => {
+    res.json({message: "/:prefix/namespaces/:namespace (delete)"})
+})
+
+// "/:prefix/namespaces/:namespace/properties (post)"
+export const v1_prefix_namespaces_namespace_properties = (req, res => {
+    res.json({message: "/:prefix/namespaces/:namespace/properties"})
+})
+
+// "/:prefix/namespaces/:namespace/tables (get)"
+export const v1_prefix_namespaces_namespace_tables_get = (req, res => {
+    res.json({message: "/:prefix/namespaces/:namespace/tables (get)"})
+})
+
+// "/:prefix/namespaces/:namespace/tables (post)"
+export const v1_prefix_namespaces_namespace_tables_post = (req, res => {
+    res.json({message: "/:prefix/namespaces/:namespace/tables (post)"})
+})
+
+// "/:prefix/namespaces/:namespace/tables/:table (get)"
+export const v1_prefix_namespaces_namespace_tables_table_get = (req, res => {
+    res.json({message: "/:prefix/namespaces/:namespace/tables (get)"})
+})
+
+// "/:prefix/namespaces/:namespace/tables/:table (post)"
+export const v1_prefix_namespaces_namespace_tables_table_post = (req, res => {
+    res.json({message: "/:prefix/namespaces/:namespace/tables (post)"})
+})
+
+// "/:prefix/namespaces/:namespace/tables/:table (delete)"
+export const v1_prefix_namespaces_namespace_tables_table_delete = (req, res => {
+    res.json({message: "/:prefix/namespaces/:namespace/tables (delete)"})
+})
+
+// "/iceberg/v1/:prefix/tables/rename"
+export const v1_prefix_tables_rename = (req, res => {
+    res.json({message: "/iceberg/v1/:prefix/tables/rename"})
+})
+
+// "/:prefix/namespaces/:namespace/tables/:table/metrics"
+export const v1_prefix_namespaces_namespace_tables_metrics = (req, res => {
+    res.json({message: "/:prefix/namespaces/:namespace/tables/:table/metrics"})
+})
--- routers/iceberg/v1/index.js
+++ routers/iceberg/v1/index.js
@@ -1,7 +1,22 @@
 // this router handles routing from "/iceberg/v1"
 
 import express from "express";
-import { v1_config } from "../../../controllers/iceberg/v1/controllers.js";
+import {
+  v1_config,
+  v1_oauth_tokens,
+  v1_prefix_namespaces_get,
+  v1_prefix_namespaces_post,
+  v1_prefix_namespaces_namespace_get,
+  v1_prefix_namespaces_namespace_delete,
+  v1_prefix_namespaces_namespace_properties,
+  v1_prefix_namespaces_namespace_tables_get,
+  v1_prefix_namespaces_namespace_tables_post,
+  v1_prefix_namespaces_namespace_tables_table_get,
+  v1_prefix_namespaces_namespace_tables_table_delete,
+  v1_prefix_namespaces_namespace_tables_table_post,
+  v1_prefix_tables_rename,
+  v1_prefix_namespaces_namespace_tables_metrics
+} from "../../../controllers/iceberg/v1/controllers.js";
 import placeholder from "../../../controllers/placeholder.js";
 
 const v1Router = express.Router();
@@ -12,73 +27,73 @@ v1Router.get("/test", (req, res) => res.send("server working"));
 // /iceberg/v1/config
 v1Router.get("/config", v1_config);
 
-// /iceberg/v1/oauth/tokens
-v1Router.get("/oauth/tokens", placeholder("/v1/oauth/tokens"));
+// /iceberg/v1/oauth/tokens - POST
+v1Router.post("/oauth/tokens", v1_oauth_tokens);
 
 // /iceberg/v1/:prefix/namespaces
-v1Router.get("/:prefix/namespaces", placeholder("/v1/:prefix/namespaces"));
+v1Router.get("/:prefix/namespaces", v1_prefix_namespaces_get);
 
 // /iceberg/v1/:prefix/namespaces - POST
-v1Router.post("/:prefix/namespaces", placeholder("/v1/:prefix/namespaces"));
+v1Router.post("/:prefix/namespaces", v1_prefix_namespaces_post);
 
 // /iceberg/v1/:prefix/namespaces/:namespace
 v1Router.get(
   "/:prefix/namespaces/:namespace",
-  placeholder("/v1/:prefix/namespaces/:namespace")
+  v1_prefix_namespaces_namespace_get
 );
 
 // /iceberg/v1/:prefix/namespaces/:namespace - DELETE
 v1Router.delete(
   "/:prefix/namespaces/:namespace",
-  placeholder("/v1/:prefix/namespaces/:namespace")
+  v1_prefix_namespaces_namespace_delete
 );
 
 // /iceberg/v1/:prefix/namespaces/:namespace/properties - POST
 v1Router.post(
   "/:prefix/namespaces/:namespace/properties",
-  placeholder("/v1/:prefix/namespaces/:namespace/properties")
+  v1_prefix_namespaces_namespace_properties
 );
 
 // /iceberg/v1/:prefix/namespaces/:namespace/tables
 v1Router.get(
-    "/:prefix/namespaces/:namespace/tables",
-    placeholder("/v1/:prefix/namespaces/:namespace/tables")
-  );
+  "/:prefix/namespaces/:namespace/tables",
+  v1_prefix_namespaces_namespace_tables_get
+);
 
 // /iceberg/v1/:prefix/namespaces/:namespace/tables - POST
 v1Router.post(
-    "/:prefix/namespaces/:namespace/tables",
-    placeholder("/v1/:prefix/namespaces/:namespace/tables")
-  );
+  "/:prefix/namespaces/:namespace/tables",
+  v1_prefix_namespaces_namespace_tables_post
+);
 
 // /iceberg/v1/:prefix/namespaces/:namespace/tables/:table
 v1Router.get(
-    "/:prefix/namespaces/:namespace/tables/:table",
-    placeholder("/v1/:prefix/namespaces/:namespace/tables/:table")
-  );
+  "/:prefix/namespaces/:namespace/tables/:table",
+  v1_prefix_namespaces_namespace_tables_table_get
+);
 
 // /iceberg/v1/:prefix/namespaces/:namespace/tables/:table - POST
 v1Router.post(
-    "/:prefix/namespaces/:namespace/tables/:table",
-    placeholder("/v1/:prefix/namespaces/:namespace/tables/:table")
-  );
+  "/:prefix/namespaces/:namespace/tables/:table",
+  v1_prefix_namespaces_namespace_tables_table_post
+);
 
 // /iceberg/v1/:prefix/namespaces/:namespace/tables/:table - DELETE
 v1Router.delete(
-    "/:prefix/namespaces/:namespace/tables/:table",
-    placeholder("/v1/:prefix/namespaces/:namespace/tables/:table")
-  );
+  "/:prefix/namespaces/:namespace/tables/:table",
+  v1_prefix_namespaces_namespace_tables_table_delete
+);
 
 // /iceberg/v1/:prefix/tables/rename - POST
 v1Router.post(
-    "/:prefix/tables/rename",
-    placeholder("/v1/:prefix/tables/rename")
-  );
+  "/:prefix/tables/rename",
+  v1_prefix_tables_rename
+);
 
 // /iceberg/v1/:prefix/namespaces/:namespace/tables/:table/metrics - POST
 v1Router.post(
-    "/:prefix/namespaces/:namespace/tables/:table/metrics",
-    placeholder("/v1/:prefix/namespaces/:namespace/tables/:table/metrics")
-  );
+  "/:prefix/namespaces/:namespace/tables/:table/metrics",
+  v1_prefix_namespaces_namespace_tables_metrics
+);
 
 export default v1Router;
